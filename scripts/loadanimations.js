7f6c161159dc1de0a8d4a601c3fdefc9d1b98d1c
RazerXP <93345419+RazerXP@users.noreply.github.com>
2024-04-08 02:20:27 +0530

    Add files via upload

---
+++ scripts/loadanimations.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function() {
+    var services = document.querySelectorAll('.card');
+    services.forEach(function(service) {
+      if (isElementPartiallyInViewport(service) && !service.classList.contains('slide-in')) {
+        service.classList.add('slide-in');
+      }
+    });
+});
+window.addEventListener('scroll', function() {
+    var services = document.querySelectorAll('.card2');
+    services.forEach(function(service) {
+      if (isElementPartiallyInViewport(service) && !service.classList.contains('sideslide-in')) {
+        service.classList.add('sideslide-in');
+      }
+    });
+});
+
+
+function isElementPartiallyInViewport(element) {
+var rect = element.getBoundingClientRect();
+var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+var threshold = 0.6; // Adjust this threshold as needed
+
+// Check if at least 50% of the element is visible
+return (
+    rect.bottom >= 0 &&
+    rect.top <= windowHeight &&
+    rect.bottom <= windowHeight * (1 + threshold) &&
+    rect.top >= -rect.height * threshold
+);
+}
